89f7ba0d67097ce06ba2f1ddee0907db40623257
jiangyu198558 <jiangyu198558@163.com>
2017-06-26 09:41:58 +0800

    增加开始界面

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function(){
+	function DomObject(dom){
+		this.dom = dom;
+	}
+
+	DomObject.prototype.get = function(){
+		return this.dom;
+	};
+
+	DomObject.prototype.on = function(eventName, eventHandler){
+		this.get().addEventListener(eventName, eventHandler);
+	}
+
+	DomObject.prototype.css = function(styleKey, styleValue){
+		this.get().style[styleKey] = styleValue;
+	}
+
+	function $(selector, context) {
+		return new DomObject((context || document).querySelector(selector));
+	}
+
+	function _init() {
+		$('#btn-start').on('click', function(ev){
+			$('.start-container').css('display', 'none');
+			$('.game-container').css('display', 'block');
+		});
+
+		$('#btn-setting').on('click', function(ev){
+			alert('You click setting btn.');
+		});
+	}
+
+	document.addEventListener('DOMContentLoaded', function(e){
+		_init();
+	});
+})(document);
