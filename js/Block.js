d21996819db1b631cd58eee8bcdb944b7b5755ee
jiangyu198558 <jiangyu198558@163.com>
2017-07-13 10:15:08 +0800

    绘制方块

---
+++ js/Block.js
@@ -0,0 +1,19 @@
+(function (window) {
+  'use strict';
+  function Block(blockType) {
+    this.blockType = blockType;
+    this.size = 30;
+    this.originalSize = 32;
+    this.sprite = window.ResourceManager.getResource('blocks');
+  }
+
+  Block.prototype = {
+    constructor: Block,
+    draw: function (context, x, y) {
+      context.drawImage(this.sprite, (this.blcokType - 1) * this.originalSize, 0, this.originalSize, this.originalSize, x * this.size, y * this.size, this.size, this.size);
+    }
+  };
+
+  window.Block = Block;
+
+})(window);
--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
-(function(){
-	function DomObject(dom){
-		this.dom = dom;
-	}
+(function (document) {
 
-	DomObject.prototype.get = function(){
-		return this.dom;
-	};
+  function DomObject(dom) {
+    this.dom = dom;
+  }
+  DomObject.prototype.get = function () {
+    return this.dom;
+  };
 
-	DomObject.prototype.on = function(eventName, eventHandler){
-		this.get().addEventListener(eventName, eventHandler);
-	}
+  DomObject.prototype.on = function (eventName, eventHandler) {
+    this.get().addEventListener(eventName, eventHandler);
+  };
 
-	DomObject.prototype.css = function(styleKey, styleValue){
-		this.get().style[styleKey] = styleValue;
-	}
+  DomObject.prototype.css = function (styleKey, styleValue) {
+    this.get().style[styleKey] = styleValue;
+  };
 
-	function $(selector, context) {
-		return new DomObject((context || document).querySelector(selector));
-	}
+  function $(selector, context) {
+    return new DomObject((context || document).querySelector(selector));
+  }
 
-	function startGame(){
-		new Board();
-	}
+  function startGame() {
+    ResourceManager.onResourceLoaded = function () {
+      new Board();
+    };
+    ResourceManager.init();
+  }
 
-	function _init() {
-		$('#btn-start').on('click', function(ev){
-			$('.start-container').css('display', 'none');
-			$('.game-container').css('display', 'block');
-			startGame();
-		});
+  function _init() {
+    $('#btn-start').on('click', function (ev) {
+      $('.start-container').css('display', 'none');
+      $('.game-container').css('display', 'block');
+      startGame();
+    });
+    $('#btn-setting').on('click', function (ev) {
+      alert('You clicked the setting button.');
+    });
+  }
 
-		$('#btn-setting').on('click', function(ev){
-			alert('You click setting btn.');
-		});
-	}
-
-	document.addEventListener('DOMContentLoaded', function(e){
-		_init();
-	});
+  document.addEventListener('DOMContentLoaded', function (ev) {
+    _init();
+  });
 })(document);
